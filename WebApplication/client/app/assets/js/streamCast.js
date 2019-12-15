c6ec7544122766e1c5d3c442419c889b43c82bef
TMele54 <tonymele.Rutgers@gmail.com>
2019-12-14 22:59:54 -0500

    login registration pages

--- WebApplication/client/app/assets/js/streamCast.js
+++ WebApplication/client/app/assets/js/streamCast.js
@@ -2,3 +2,12 @@ function refreshIframe() {
     var ifr = document.getElementsByName('Right')[0];
     ifr.src = ifr.src;
 }
+
+function myHandler(e) {
+    $("#myVideo").fadeOut(1000);
+    $("#loginForm").fadeIn(3000);
+}
+
+setTimeout(function (){
+    myHandler("")
+ }, 3000)
